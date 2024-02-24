667683476ca968973ea54411193f64989547a39e
yairaberrio <161092887+yairaberrio@users.noreply.github.com>
2024-02-23 20:58:11 -0500

    Add files via upload

---
+++ ensayo2.py
@@ -0,0 +1,10 @@
+#se requiere el sueldo de untrabajador con base en las horas
+
+ht = 47
+ph = 6800
+sm = 4
+nombre = input("ingrese su nombre:")
+ss = ht * ph * sm
+print(f"el salario de {nombre} es {ss}")
+print(nombre)
+
